f62f12aded111379bdc89d2d700c3a80cfbb9ded
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-05-31 11:52:29 +0530

    added

--- Index.js
+++ Index.js
@@ -1,27 +1,25 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-require('dotenv').config(); 
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
-app.get('/api/sample-data', async (req, res) => {
+app.get("/api/sample-data", async (req, res) => {
   try {
- 
     const response = await axios.get(process.env.API_URL);
-    
+
     console.log(response);
     res.json(response.data);
   } catch (error) {
-    console.error('Error fetching data:', error);
-    
-    res.status(500).json({ error: 'Failed to fetch data' });
+    console.error("Error fetching data:", error);
+
+    res.status(500).json({ error: "Failed to fetch data" });
   }
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- README.md
+++
@@ -1,1 +0,0 @@
-# Boxigo-Backend
